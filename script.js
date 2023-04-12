acb61d009482487a23e829b0b1d6a3382327f0e0
Timur Lagawy <99760868+TimurLagawy@users.noreply.github.com>
2023-04-12 17:22:47 +0200

    refactor: refactoring some code from main

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-let petCards = document.querySelectorAll(".pepCard");
-let currentPetCard = 0;
-let isEnaible = true;
+let items = document.querySelectorAll(".item");
+let currentItem = 0;
+let isEnaibled = true;
 
-function changeCurrentPetCard(n) {
-  currentPetCard = (n + petCards.length) % petCards.length;
+function changeCurrentItem(n) {
+  currentItem = (n + items.length) % items.length;
 }
 
-document
+function hideItem(direction) {
+  isEnaibled = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener("animationend", function () {
+    this.classList.remove("active", direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add("next", direction);
+  items[currentItem].addEventListener("animationend", function () {
+    this.classList.remove("next", direction);
+    this.classList.add("active");
+    isEnaibled = true;
+  });
+}
+
+function previosItem(n) {
+  hideItem("to-left");
+  changeCurrentItem(n + 1);
+  showItem("from-right");
+}
+function nextItem(n) {
+  hideItem("to-right");
+  changeCurrentItem(n - 1);
+  showItem("from-left");
+}
+document.querySelector(".control.left").addEventListener("click", function () {
+  if (isEnaibled) {
+    previosItem(currentItem);
+  }
+});
+
+document.querySelector(".control.right").addEventListener("click", function () {
+  if (isEnaibled) {
+    nextItem(currentItem);
+  }
+});
